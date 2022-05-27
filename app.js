067faa01dc6586206879533337261050e668bab9
Priyansh <priyanshkumar205@gmail.com>
2022-05-27 23:57:00 +0530

    updated site

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const bodyparser = require("body-parser");
 const request = require("request");
 
 
-
 const app = express();
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html");
+});
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
